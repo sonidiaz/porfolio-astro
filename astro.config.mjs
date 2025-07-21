83498669b5d76e05e689a2070579e11b491668a2
NachoDíaz <nachodiaz8@gmail.com>
2025-07-21 10:44:17 +0200

    update config app

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config'
 import tailwind from "@astrojs/tailwind"
 import robotsTxt from "astro-robots-txt"
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,5 +9,12 @@ export default defineConfig({
       tailwind(), 
       robotsTxt()
   ],
-  site: 'https://porfolio.dev/'
+  site: 'https://nachodiaz.me',
+  output: 'server',
+  adapter: vercel({
+    analytics: true,
+    webAnalytics: {
+      enabled: true
+    }
+  })
 })
